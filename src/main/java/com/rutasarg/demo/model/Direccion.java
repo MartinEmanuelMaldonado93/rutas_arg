2735d9461591850213f183f660fdacec44c83938
Jano Zeballos <janozeballos97@gmail.com>
2023-11-13 08:52:26 -0300

    Fix ID de entidad Direccion

--- src/main/java/com/rutasarg/demo/model/Direccion.java
+++ src/main/java/com/rutasarg/demo/model/Direccion.java
@@ -13,12 +13,14 @@ public class Direccion {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idDestinatario;
-    private String nombre;
-    private String apellido;
-    private String nroDocumento;
-    private String telefono;
-    private String email;
+    private Long idDireccion;
+    private String calle;
+    private String altura;
+    private String piso;
+    private String departamento;
+    private String codigoPostal;
+    private String comentario;
+    private String localidad;
     @OneToMany(cascade = CascadeType.ALL)
     private List<Paquete> paquete;
     
